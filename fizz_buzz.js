2c54025dfe1a31a30c6f6e41a23b4dd90bb03749
Gorka <doctor.scissor@gmail.com>
2016-05-31 00:07:53 +0500

    try ssh

--- fizz_buzz.js
+++ fizz_buzz.js
@@ -5,10 +5,9 @@ for (var i = 0; i < 100; i++) {
 var str = '';
 exports.fizzbuzz_string = function() {
 	arr.forEach(function(i){
-	if (i%15 == 0) {str += (' ' + 'FizzBuzz' + ' ')}
-		else if(i%5 == 0) {str += (' ' + 'Buzz' + ' ')}
-			else if(i%3 == 0) {str += (' ' + 'Fizz' + ' ')}
-			else {str += (' ' + i + ' ')};
+	(i%15 == 0) ? str += 'FizzBuzz' : (i%5 == 0) ? str += 'Buzz':
+		(i%3 == 0) ? str += 'Fizz': (str += i);
+	str += ' ';
 	});
 	console.log(str);
 };  
